94ece8b60467bcbc958871e4fbc3be67005975c8
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-05 08:17:33 -0300

    paginação de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -22,6 +22,15 @@ router.post('/categories/save', (req, res) => {
     res.redirect('/admin/categories/new');
   }
 
+});
+
+router.get('/admin/categories', (req, res) => {
+
+  Category.findAll().then(categories => {
+    res.render('admin/categories/index', {
+      categories: categories
+    });
+  })
 
 });
 
