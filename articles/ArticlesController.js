55bb6ac7a47ae2af405da7f9df5d3c367fae17e0
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-08 08:58:54 -0300

    Persistindo dados do arquivo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('../articles/Article');
+const slugify = require('slugify');
 
 router.get('/admin/articles/new', (req, res) => {
-  res.render('admin/articles/new')
+  Category.findAll().then(categories => {
+    res.render('admin/articles/new', {categories})
+  })
+})
+
+router.post('/articles/save', (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const categoryId = req.body.category;
+
+  Article.create({
+    title,
+    slug: slugify(title, {lower: true}),
+    body,
+    categoryId
+  })
 })
 
 module.exports = router;
