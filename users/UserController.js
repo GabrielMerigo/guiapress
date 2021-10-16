cd7c53cdc521af828ebc0f2c243a586dc71b2676
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-16 12:53:25 -0300

    Update UserController.js

--- users/UserController.js
+++ users/UserController.js
@@ -14,9 +14,10 @@ router.get('/admin/users/create', (req, res) => {
 router.post('/users/create', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  
 
   const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt)
+  const hash = bcrypt.hashSync(password, salt);
 
   User.create({
     email,
