70eb6dbd2de80db68000a427c01d1b205cc0b915
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-16 13:58:59 -0300

    Sessões & Cookies

--- users/UserController.js
+++ users/UserController.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const User = require('./User');
 const bcrypt = require('bcryptjs');
 
-router.get('/admin/users', (req, res) => {
-  res.send('Listagem de usuários')
-});
-
 router.get('/admin/users/create', (req, res) => {
   res.render('admin/users/create');
+});
+
+router.get('/admin/users/', (req, res) => {
+  User.findAll().then(users => {
+    res.render('admin/users/index', { users })
+  });
 });
 
 router.post('/users/create', (req, res) => {
