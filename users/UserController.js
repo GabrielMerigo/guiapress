29036ea9ef660d539bf0ee30868d3ad595ef823f
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-19 09:19:32 -0300

    autenticação feita!

--- users/UserController.js
+++ users/UserController.js
@@ -43,4 +43,43 @@ router.post('/users/create', (req, res) => {
   })
 });
 
+function welcome() {
+  console.log('oi');
+}
+
+router.get('/login', (req, res) => {
+  res.render('admin/users/login');
+})
+
+router.post('/autenticate', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ 
+    where: {
+      email: email
+    }
+  }).then(user => {
+    if(user){
+      //Validar senha
+      const corret = bcrypt.compareSync(password, user.password);
+      if(corret){
+        req.session.user = {
+          id: user.id,
+          email: user.email
+        }
+
+        res.json(req.session.user);
+      }else{
+        res.redirect('/login');
+      }
+
+    }else{
+      res.redirect('/login');
+    }
+  })
+
+
+});
+
 module.exports = router;
